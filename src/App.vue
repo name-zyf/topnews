5f2adc99837002182eefbbcf1085c8a5d454252c
name-zyf <550769324@qq.com>
2019-10-26 10:46:36 +0800

    安装并测试UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <el-***>这是全局下已经注册好了的组件，不需要额外（注册）挂载了，可以直接使用 -->
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.el-card {
+  width: 480px;
+  height: 42px;
+  position: absolute;
+  left: 0;
+  top: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 全局导入UI插件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false // 生产提示为ture则提示会少一些
+/**
+ * 使用插件【注意】这个操作相当于全局安装了UI，
+ * 换句话说，就不用额外的再在Vue实例上的components:{}上再注册了，可以直接使用了
+ */
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+/**
+ *  main.js文件职责
+ *  1.创建根实例
+ *  2.赋值全局范围的依赖导入
+ */
